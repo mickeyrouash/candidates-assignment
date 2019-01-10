02b887e88365207b0751bb9c4c91c5e4b0b23f97
Mickey Roash <mickey.rouash@gmail.com>
2019-01-10 16:29:10 +0200

    exercises solustion

--- vagrant/fixScripts/exercise5-fix_server1.sh
+++ vagrant/fixScripts/exercise5-fix_server1.sh
@@ -3,4 +3,3 @@
 sudo apt-get install -y sshpass
 su  - vagrant -c "ssh-keygen -f ~/.ssh/id_rsa -t rsa -N ''"
 su  - vagrant -c  "echo 'ssh-keyscan -H server2 >> ~/.ssh/known_hosts && sshpass -p `echo dmFncmFudAo= |base64 -d` ssh-copy-id   vagrant@server2' >> ~vagrant/setSshNoPass.sh"
-echo  'exit 0' >> /etc/rc.local
